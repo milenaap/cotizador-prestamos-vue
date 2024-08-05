296c54f6adf4f8d81f31db9ab175b59515b09f49
mile <darimile@gmail.com>
2024-08-05 21:28:26 +0200

    add Button

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,114 @@
 <script setup>
-    import { ref, reactive } from 'vue';
+    import { ref, computed } from 'vue';
     import Header from "./components/Header.vue";
+    import Button from './components/Button.vue';
 
-    const cantidad = ref(0);
+    const cantidad = ref(10000);
+    const meses = ref(6);
+    const MIN = 0;
+    const MAX = 20000;
+    const STEP = 100;
 
-    const state = reactive({
-        cantidad: 0
-    })
+    const formatearDinero = computed(() => {
+        const formatter = new Intl.NumberFormat('es-ES', {
+            style: 'currency',
+            currency: 'EUR'
+        });
 
-    function handleChange(e){
-        console.log(e.target.value);
+        return formatter.format(cantidad.value);
+    });
+
+    const handleChangeDecremento = () => {
+        const valor = cantidad.value - STEP
+        if(valor < MIN) {
+            alert('Cantidad no válida');
+            return;
+        }
+        cantidad.value = valor;
+    }
+    const handleChangeIncremento = () => {
+        const valor = cantidad.value + STEP
+        if(valor > MAX) {
+            alert('Cantidad no válida');
+            return;
+        }
+        cantidad.value = valor;
     }
 
-    console.log(cantidad.value); // cuando usamos ref
+    // const state = reactive({
+    //     cantidad: 0
+    // })
+
+    // function handleChange(e){
+    //     cantidad.value = Number(e.target.value);
+    //     // state.cantidad = Number(e.target.value);
+    // }
+
+    // console.log(cantidad.value); // cuando usamos ref
     
-    console.log(state.cantidad); // cuando usamos reactive
+    // console.log(state.cantidad); // cuando usamos reactive
 
 
 </script>
 
 <template>
 <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
-    <Header />
+    <!-- Asi se llaman los componentes <nombreDelComponente /> -->
+    <Header /> 
+
+    <div class="flex justify-between mt-10">
+       
+        <Button 
+            :operador="'-'"
+            @fn="handleChangeDecremento" 
+        />
+            
+        
+        <Button
+            :operador="'+'"
+            @fn="handleChangeIncremento"
+        />
+            
+    </div>
 
     <div class="my-5">
         <input 
         type="range"
         class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600 "
-        @input="handleChange"
+        :min="MIN"
+        :max="MAX"
+        :step="STEP"
+        v-model.number="cantidad"
+        />
+
+        <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">{{ formatearDinero }}</p>
+
+        <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+            Elige un <span class="text-indigo-600">Plazo </span> a pagar
+        </h2>
+
+        <select
+            class="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-lg font-bold text-gray-500 mt-5"
+            :value="meses"
+            v-model.number="meses"
+
         >
+            <option value="6">6 Meses</option>
+            <option value="12">12 Meses</option>
+            <option value="24">24 Meses</option>
+        </select>
+         <!-- <p v-text="`$ ${cantidad}`"></p> -->
+        
+    </div>
+
+    <div class="my-5 space-y-3 bg-gray-50 p-5">
+        <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+            Resumen <span class="text-indigo-600">de pagos</span>
+        </h2>
+
+        <p class="text-xl text-500 text-center font-bold">{{meses}} Meses</p>
+        <p class="text-xl text-500 text-center font-bold">total a pagar:</p>
+        <p class="text-xl text-500 text-center font-bold">Mensuales</p>
     </div>
 
 </div>
---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button
+        type="button"
+        class="h-10 w-10 flex items-center justify-center fond-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2Xl"
+        @click="$emit('fn')"
+    >{{operador}}</button>
+</template>
+
+<script setup>
+
+    const props = defineProps({
+        operador: String,
+        
+    });
+
+</script>
+
+
